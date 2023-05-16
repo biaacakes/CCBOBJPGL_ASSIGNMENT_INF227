59f424ee7c18e542881e3b4ad35dee8f95381b99
Bianca Reine Betos <120092488+biaacakes@users.noreply.github.com>
2023-05-16 23:05:30 +0800

    Create App.java

---
+++ EXERCISE3/Dog/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Create object from pet class
+        GoldenChow myPet = new GoldenChow();
+
+        myPet.play();
+        System.out.println(myPet.faceShape);
+        System.out.println(myPet.personality);
+    }
+}
